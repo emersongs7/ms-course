a5b171ccde3c5266f3644543748014118e1983ab
Emerson <emersonsouza@brq.com>
2021-06-17 21:44:28 -0300

    Login, JWT token

---
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/config/AuthorizationServerConfig.java
@@ -0,0 +1,61 @@
+package com.devsuperior.hroauth.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration					// indica que uma classe declara um ou mais métodos @Bean e pode ser processada pelo contêiner Spring para gerar definições
+								// de bean e solicitações de serviço para esses beans em tempo de execução. ... Isso é chamado de recurso Spring Java Config (usando a anotação @Configuration).
+@EnableAuthorizationServer		// Essa annotation ela faz um pré processamento por baixo dos panos, configurando o nosso MS como um AuthorizationServer do OAUTH
+
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter{
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private JwtAccessTokenConverter accessTokenConverter;
+	
+	@Autowired
+	private JwtTokenStore tokenStore;
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+		security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
+		super.configure(security);
+	}
+
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory()
+		.withClient("myappname123")		
+		.secret(passwordEncoder.encode("myappsecret123"))
+		.scopes("read", "write")
+		.authorizedGrantTypes("password")
+		.accessTokenValiditySeconds(86400);
+		super.configure(clients);
+	}
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.authenticationManager(authenticationManager)
+		.tokenStore(tokenStore)
+		.accessTokenConverter(accessTokenConverter);
+		super.configure(endpoints);
+	}
+	
+	
+	
+
+}
